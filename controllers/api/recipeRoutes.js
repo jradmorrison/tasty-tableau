b186a748ac8815c5fffb6990211d7cb0e8e75c30
Brandon Barnes <Imbrandonbarnes@gmail.com>
2023-11-07 23:36:24 -0500

    Compleated Seeding for DB Json Files

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -12,10 +12,7 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-res.status(200).json(recipeData);
-    } catch (err) {
-    res.status(500).json(err);
-}
+  res.status(200).json(recipeData);
 });
 
 // Get recipe by ID
@@ -42,10 +39,10 @@ router.get('/:id', async (req, res) => {
       res.status(404).json({ message: `No recipes found for user with id: ${id}.` });
       return;
     }
-        if (!recipeData) {
-            res.status(404).json({ message: `No recipes found for user with id: ${id}.` });
-            return;
-        };
+    if (!recipeData) {
+      res.status(404).json({ message: `No recipes found for user with id: ${id}.` });
+      return;
+    }
 
     res.status(200).json(recipeData);
   } catch (err) {
