9526da55945d8138d56cbee0ac265422db10b84f
Brandon Barnes <Imbrandonbarnes@gmail.com>
2023-11-09 15:46:16 -0500

    Added Username to top right and added login to save to recipes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -41,6 +41,7 @@ router.get('/', async (req, res) => {
 // Get recipe by ID
 router.get('/recipes/:id', async (req, res) => {
   try {
+    console.trace('test1');
     const recipeData = await Recipe.findByPk(req.params.id, {
       include: [
         {
@@ -57,14 +58,16 @@ router.get('/recipes/:id', async (req, res) => {
       ],
     });
 
-    const favoritesData = await Favorite.findAll({
-      where: {
-        recipe_id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    const isFavorite = favoritesData.length > 0;
+    let favorite = -1;
+    if (req.session.user_id) {
+      const favoritesData = await Favorite.findAll({
+        where: {
+          recipe_id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+      favorite = favoritesData.length > 0 ? 1 : 0;
+    }
     const recipe = recipeData.get({ plain: true });
 
     recipe.images = recipe.images.split(', ')[0].slice(1);
@@ -73,12 +76,12 @@ router.get('/recipes/:id', async (req, res) => {
     }
 
     recipe.instructions = recipe.instructions.slice(1, -1);
-    console.log(recipe);
+    console.log(favorite);
     res.render('recipe', {
       recipe,
       logged_in: req.session.logged_in,
-      // user: req.session.username,
-      is_favorite: isFavorite,
+      user: req.session.username,
+      is_favorite: favorite,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -90,22 +93,21 @@ router.get('/dashboard', withAuth, async (req, res) => {
     const userRecipesData = await Recipe.findAll({
       where: {
         user_id: req.session.user_id,
-
-      }
-
+      },
     });
     const favoritesData = await Favorite.findAll({
       where: {
         user_id: req.session.user_id,
       },
-      include: [{
-        model: Recipe,
-      }]
+      include: [
+        {
+          model: Recipe,
+        },
+      ],
     });
 
-    const userRecipes = userRecipesData.map(rec => rec.get({ plain: true }));
-    const favorites = favoritesData.map(fav => fav.get({ plain: true }));
-  
+    const userRecipes = userRecipesData.map((rec) => rec.get({ plain: true }));
+    const favorites = favoritesData.map((fav) => fav.get({ plain: true }));
 
     // Grabs the first image and creates a new attribute for it
     userRecipes.forEach((recipe) => {
@@ -114,11 +116,11 @@ router.get('/dashboard', withAuth, async (req, res) => {
         recipe.image = recipe.image.slice(0, recipe.image.length - 1);
       }
     });
-    favorites.forEach(favorite => {
+    favorites.forEach((favorite) => {
       favorite.recipe.image = favorite.recipe.images.split(', ')[0].slice(1);
       if (favorite.recipe.image.charAt(favorite.recipe.image.length - 1) === ']') {
         favorite.recipe.image = favorite.recipe.image.slice(0, favorite.recipe.image.length - 1);
-      };
+      }
     });
 
     userRecipes.reverse();
@@ -166,7 +168,7 @@ router.get('/newrecipe', withAuth, async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
 
 // Login form
 router.get('/login', (req, res) => {
--- public/js/favorite.js
+++ public/js/favorite.js
@@ -30,6 +30,12 @@ $(function () {
     }
   };
 
+  const goToLogin = () => {
+    document.location.replace('/login');
+    console.trace('Click');
+  };
+
   $('.add-to-favorites-btn').on('click', updateFavorites);
   $('.remove-from-favorites-btn').on('click', updateFavorites);
+  $('#login-to-save').on('click', goToLogin);
 });
