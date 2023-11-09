f95c1dc1ad6941a026fa4a43ad0427364a6ce8cc
Kurt Riecken <kurt.riecken@gmail.com>
2023-11-09 10:06:30 -0500

    added favorites button and popover to indicate added

---
+++ public/js/favorite.js
@@ -0,0 +1,31 @@
+$(function() {
+
+    $('[data-toggle="popover"]').popover();
+
+    const addToFavorites = async (event) => {
+        event.preventDefault();
+
+        const id = event.target.dataset.id;
+        console.log(event.target.dataset.id);
+
+        if (id) {
+            const response = await fetch(`../api/favorites/user/${id}`, {
+                method: 'POST',
+                body: '',
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            const data = await response.json();
+
+            console.log(data);
+        }
+
+
+    }
+
+
+
+
+
+    $('.add-to-favorites-btn').on('click', addToFavorites);
+})
