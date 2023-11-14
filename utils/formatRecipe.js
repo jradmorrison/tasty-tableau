a0acd394965a840986f7e9670ae904379e5b355d
jradmorrison <142756649+jradmorrison@users.noreply.github.com>
2023-11-14 12:24:28 -0500

    added comments to util functions

--- utils/formatRecipe.js
+++ utils/formatRecipe.js
@@ -1,14 +1,13 @@
 const formatTime = require('./formatTime');
 
+// Grabs the first image from the string of images and formats the time required
 const formatRecipe = (recipe) => {
-  // console.trace(recipe.images);
     recipe.image = recipe.images.split(', ')[0].slice(1);
-    // console.trace(recipe.image);
 
     if (recipe.image.charAt(recipe.image.length - 1) === ']') {
       recipe.image = recipe.image.slice(0, recipe.image.length - 1);
     }
-    // console.trace(recipe.time_total);
+
     if (!recipe.time_total.startsWith('PT') || recipe.time_total == 'PT0S' || recipe.time_total == 'PT') {
       recipe.time_total = 'Cook time not available'
     }
@@ -16,7 +15,6 @@ const formatRecipe = (recipe) => {
       recipe.time_total = recipe.time_total.slice(2);
       recipe.time_total = formatTime(recipe.time_total);
     }
-    // console.trace(recipe);
 
     return recipe;
 };
